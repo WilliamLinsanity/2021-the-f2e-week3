70f76233f9aa8fb0787b350f5d6dec085d63ecf8
WilliamLinsanity <s3211t@gmail.com>
2021-11-29 00:20:33 +0800

    update method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import { Route } from 'react-router-dom';
-import { Routes,BrowserRouter } from "react-router-dom";
-// import Bus from './components/Bus'
+import { Routes, HashRouter } from "react-router-dom";
+import Bus from './components/Bus'
 import Search from './components/Search';
 import Home from './components/Home';
 import Stop from './components/Stop';
@@ -10,15 +10,15 @@ import StopMap from './components/StopMap';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home/>} />
           {/* <Route path="/bus" element={<Bus/>} /> */}
           <Route path="/search/:selectedCounty" element={<Search/>} />
-          <Route path="/stop/:selectedCounty/:routeUID" element={<Stop/>} />
-          <Route path="/stop/map/:selectedCounty/:routeUID" element={<StopMap/>} />
+          {/* <Route path="/stop/:selectedCounty/:routeUID/:routeName" element={<Stop/>} />
+          <Route path="/stop/map/:selectedCounty/:routeUID/:routeName" element={<StopMap/>} /> */}
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
